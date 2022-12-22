815b5610678f63606e1ae10c7e68d17c87bc9f37
Beatriz Lira <beatrizliragonzaga@gmail.com>
2022-12-22 16:00:51 -0300

    :tada: initial commit

---
+++ src/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+
+import { GlobalStyles } from './styles/global-styles';
+import { theme } from './styles/theme';
+import Home from './templates/Home';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Switch>
+          <Route to="*" component={Home} />
+        </Switch>
+      </BrowserRouter>
+      <GlobalStyles />
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
---
+++ src/templates/Home/index.jsx
@@ -0,0 +1,11 @@
+import * as Styled from './styles';
+
+import { Heading } from '../../components/Heading';
+
+import config from '../../config';
+
+function Home() {
+  return <Heading>ola mundo</Heading>;
+}
+
+export default Home;
