9e79988dc80409274eca13270f09b432cc389f5a
Beatriz Lira <beatrizliragonzaga@gmail.com>
2022-12-27 21:07:04 -0300

    :white_check_mark: generate snapshot from Heading component

--- src/components/Heading/Heading.test.jsx
+++ src/components/Heading/Heading.test.jsx
@@ -93,4 +93,9 @@ describe('<Heading />', () => {
 
     expect(h6.tagName.toLowerCase()).toBe('h6');
   });
+
+  it('should match snapshot', () => {
+    const { container } = renderTheme(<Heading>texto</Heading>);
+    expect(container).toMatchSnapshot();
+  });
 });
