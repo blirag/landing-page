f25bf97808c00a66facc81e6940d21acc9e9b5fb
Beatriz Lira <beatrizliragonzaga@gmail.com>
2023-01-20 22:51:32 -0300

    :sparkles: create GridText component with tests

---
+++ src/components/GridText/index.jsx
@@ -0,0 +1,40 @@
+import P from 'prop-types';
+import * as Styled from './styles';
+import { SectionBackground } from '../SectionBackground';
+import { Heading } from '../Heading';
+import { Text } from '../Text';
+
+export const GridText = ({ title, description, grid, background = false }) => {
+  return (
+    <SectionBackground background={background}>
+      <Styled.Container>
+        <Heading size="huge" colorDark={!background}>
+          {title}
+        </Heading>
+        <Text>{description}</Text>
+        <Styled.Grid>
+          {grid.map((el) => (
+            <Styled.GridElement key={el.title}>
+              <Heading size="medium" colorDark={!background}>
+                {el.title}
+              </Heading>
+              <Text>{el.description}</Text>
+            </Styled.GridElement>
+          ))}
+        </Styled.Grid>
+      </Styled.Container>
+    </SectionBackground>
+  );
+};
+
+GridText.propTypes = {
+  background: P.bool,
+  title: P.string.isRequired,
+  description: P.string.isRequired,
+  grid: P.arrayOf(
+    P.shape({
+      title: P.string.isRequired,
+      description: P.string.isRequired,
+    }),
+  ).isRequired,
+};
